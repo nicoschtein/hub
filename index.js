a86e3d20849f955421e291e7fd2f4140955a85aa
Chris Dukakis <chrisdukakis@gmail.com>
2017-06-22 14:34:03 +0300

    add sweeping, fix issues

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const Hub = require('./lib/hub');
 const RedisAdapter = require('./lib/adapter/redis');
 
 const hub = Hub({
-  'store': new RedisAdapter()
+  'store': new RedisAdapter(),
+  'seed': 'QWERTY9',
+  'security': 2
 });
