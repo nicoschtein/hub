16e9a360c6d2776995264219649ba4fe63546e98
Chris Dukakis <chrisdukakis@gmail.com>
2017-06-26 15:13:36 +0300

    fix bugs

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const hub = Hub({
   'seed': 'QWERTY9',
   'security': 2
 });
+
+const init = async () => {
+  
+  const testHub = await hub.create(0, 'ABCDEF9', 2, 'test');
+
+  console.log(testHub);
+
+  await hub.attach(testHub);
+
+  const account = await hub.registerAccount(0, 0, 'chris');
+
+  console.log(account);
+
+}
+
+init();
