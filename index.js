355f8880b6f47496ede6e66a0175b27e658d1d61
Chris Dukakis <chrisdukakis@gmail.com>
2017-07-16 18:43:15 +0300

    export Hub and store in entry point

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const Hub = require('./lib/hub');
-const RedisAdapter = require('./lib/adapter/redis');
+const Hub = require('./lib/hub.js');
+Hub.store = require('./lib/adapter/redis.js');
 
-const hub = Hub({
-  'store': new RedisAdapter(),
-  'seed': 'QWERTY9',
-  'security': 2
-});
-
-const init = async () => {
-  
-  const testHub = await hub.create(0, 'ABCDEF9', 2, 'test');
-
-  console.log(testHub);
-
-  await hub.attach(testHub);
-
-  const account = await hub.registerAccount(0, 0, 'chris');
-
-  console.log(account);
-
-}
-
-init();
+module.exports = Hub;
